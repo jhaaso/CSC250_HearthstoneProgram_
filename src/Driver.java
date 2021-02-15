d0a6dc293128266f9c03d3452df713983f0e1ad5
jhaaso <justin.haas@cuw.edu>
2021-02-14 21:52:07 -0600

    initial commit

--- src/Driver.java
+++ src/Driver.java
@@ -1,11 +1,18 @@
-
 public class Driver 
 {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("hello world");
-
+		hs c1 = new hs("Charmander", 1, 2, 3);
+		hs c2 = new hs("Squirtle", 1, 3, 2);
+		hs c3 = new hs("Bulbasaur", 1, 4, 1);
+		hs c4 = new hs("Pikachu", 5, 10, 8);
+		
+		c1.display();
+		c2.display();
+		c3.display();
+		c4.display();
+		
 	}
 
 }
---
+++ src/hs.java
@@ -0,0 +1,22 @@
+public class hs
+{
+	private int cost;
+	private int attack;
+	private int defense;
+	private String name;
+	
+	
+	public hs(String name, int cost, int attack, int defense)
+	{
+		this.cost = cost;
+		this.attack = attack;
+		this.defense = defense;
+		this.name = name;
+	}
+	
+	
+	void display()
+	{	
+		System.out.format("Name: %s \nCost: %d \nAttack: %d Defense: %d\n", this.name, this.cost, this.attack,this.defense);
+	}
+}
